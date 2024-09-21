0c2969f338097e95c3cd12842f53caf8e70a5658
Alexandre Lindsey <joueur.galactic@gmail.com>
2024-09-21 22:22:16 +0200

    Added emojiName()

--- src/syscall.ts
+++ src/syscall.ts
@@ -121,5 +121,23 @@ export default {
                 throw new InvalidFormat(lineNumber, `First argument of define must be a single word. Expected 1 word but got ${arg.split(" ").length} words`);
             return new StringValue(arg);
         }
+    },
+    'emojiName': {
+        syscallId: 'is.workflow.actions.getnameofemoji',
+        preprocessor: (args: Value[], lineNumber: number) => {
+            if (args.length != 1)
+                throw new SycallArgumentNumberMismatch(lineNumber, 'emojiName', 1, args.length);
+            if (!(args[0] instanceof StringValue))
+                throw new InvalidType(lineNumber, StringValue.typeName, args[0].typeName, 'First argument of emojiName must be a string.')
+            let arg = args[0].str;
+            arg.trim;
+            if (arg.includes(" "))
+                throw new InvalidFormat(lineNumber, `First argument of emojiName must be a single word. Expected 1 word but got ${arg.split(" ").length} words`);
+            const regex_emoji = /[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}]/u;
+            if (regex_emoji.test(arg))
+                throw new InvalidFormat(lineNumber, `First argument of emojiName must be an emoji. Expected emoji but got ${arg}`)
+            return new StringValue(arg);
+
+        }
     }
 }
