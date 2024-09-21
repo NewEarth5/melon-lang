69190430c5ff7dad49354d09ff824be7697edf95
Alexandre Lindsey <joueur.galactic@gmail.com>
2024-09-21 23:10:47 +0200

    Updated emojiName()

--- src/syscall.ts
+++ src/syscall.ts
@@ -130,7 +130,7 @@ export default {
             if (!(args[0] instanceof StringValue))
                 throw new InvalidType(lineNumber, StringValue.typeName, args[0].typeName, 'First argument of emojiName must be a string.')
             const regex_emoji = /[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}]/ug;
-            let arg = args[0].value.match(regex_emoji);
+            let arg = args[0].value.matchAll(regex_emoji);
             return new ListValue(arg)
             if (!arg || arg.length != 1)
                 throw new InvalidFormat(lineNumber, `First argument of emojiName must be a single emoji. Expected 1 emoji but got ${(!arg)?"no":arg.length} emojis`);
